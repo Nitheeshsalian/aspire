fa6f6a4cc195d0b02e8844e2f7e657f5c8ed284b
Nitheesh Salian <nitheeshsalian@Nitheeshs-MacBook-Air.local>
2022-04-18 23:35:26 +0530

    debit card ui created

--- src/components/debitCardScreen.js
+++ src/components/debitCardScreen.js
@@ -3,26 +3,64 @@ import { View, Image, ScrollView } from 'react-native';
 import styles from './debitStyle';
 import Text from '../utils/text'; //Custom text component where default font style have been set.
 const logo = require('../assets/images/Logo.png');
+const hideEye = require('../assets/images/Group.png');
+const aspireLogo = require('../assets/images/AspireLogo.png');
+const visaLogo = require('../assets/images/VisaLogo.png');
 
 export function DebitCardScreen() {
   return (
-    <View style={styles.container}>
-      <View style={styles.headerWrapper}>
-        <Text style={styles.headerText}>Debit Card</Text>
-        <Image
-          style={styles.logo}
-          source={logo}
-        />
-      </View>
-      <View style={{ marginTop: 24 }}>
-        <Text styles={styles.availableText}>Available balance</Text>
-        <View style={styles.balanceWrapper}>
-          <View style={styles.balanceSqaure}>
-            <Text style={styles.s$}>S$</Text>
+    <React.Fragment>
+
+      <View style={styles.container}>
+        <View style={styles.headerWrapper}>
+          <Text style={styles.headerText}>Debit Card</Text>
+          <Image
+            style={styles.logo}
+            source={logo}
+          />
+        </View>
+        <View style={{ marginTop: 24 }}>
+          <Text styles={styles.availableText}>Available balance</Text>
+          <View style={styles.balanceWrapper}>
+            <View style={styles.balanceSqaure}>
+              <Text style={styles.s$}>S$</Text>
+            </View>
+            <Text style={styles.balanceText}>3,000</Text>
           </View>
-          <Text style={styles.balanceText}>3,000</Text>
         </View>
       </View>
-    </View>
+
+      <View style={styles.cardWrapper}>
+        <View style={styles.greenCard}>
+          <View style={styles.aspireLogoRow}>
+            <Image
+              style={styles.aspireLogo}
+              source={aspireLogo}
+            />
+          </View>
+          <Text style={styles.nameText}>Mark Henry</Text>
+
+          <Text style={styles.cardNumberText}>5 6 4 7    3 4 1 1    2 4 1 3    2 0 2 0</Text>
+
+          <Text style={styles.validityText}>Thru: 12/20     CVV: 456</Text>
+          <View style={styles.visaRow}>
+            <Image
+              style={styles.visaLogo}
+              source={visaLogo}
+            />
+          </View>
+        </View>
+        <View style={styles.hideCardContent}>
+          <Image
+            style={styles.eyeImage}
+            source={hideEye}
+          />
+          <Text style={styles.hideText}>Hide card number</Text>
+        </View>
+        <Text>
+          nojfdmnvkjdfmvjkdfm
+        </Text>
+      </View>
+    </React.Fragment>
   );
 }
--- src/components/debitStyle.js
+++ src/components/debitStyle.js
@@ -34,7 +34,30 @@ const styles = StyleSheet.create({
     },
     balanceWrapper: { flexDirection: 'row', alignItems: 'center' },
     s$: { fontWeight: 'bold', fontSize: 12 },
-    balanceText: { fontSize: 24, marginTop: 15, marginLeft: 10, fontWeight: 'bold' }
+    balanceText: { fontSize: 24, marginTop: 15, marginLeft: 10, fontWeight: 'bold' },
+    cardWrapper: { position: 'absolute', top: 280, backgroundColor: '#fff', width: '100%', borderTopStartRadius: 20, borderTopRightRadius: 20, height: '100%' },
+    hideCardContent: {
+        backgroundColor: '#ffff', width: 150, borderTopStartRadius: 10,
+        borderTopRightRadius: 10, height: 44, position: 'absolute',
+        top: -80, right: 20, zIndex: 0, alignItems: 'center',
+        flexDirection: 'row', justifyContent: 'center',
+    },
+    eyeImage: { height: 16, width: 16, marginBottom: 10 },
+    hideText: { color: '#01D167', paddingLeft: 5, marginBottom: 10, fontFamily: 'Avenir Next', fontWeight: '600', fontSize: 12 },
+    greenCard: { backgroundColor: '#01D167', margin: 20, borderRadius: 10, height: 220, marginTop: -50, zIndex: 1 },
+    aspireLogoRow: { flexDirection: 'row', alignItems: 'center', justifyContent: 'flex-end', paddingRight: 24, paddingTop: 24 },
+    aspireLogo: { height: 21, width: 74 },
+    nameText: {
+        fontSize: 22, fontWeight: 'bold', paddingLeft: 24
+    },
+    cardNumberText: {
+        fontSize: 14, fontWeight: '600', paddingLeft: 24, paddingTop: 24
+    },
+    validityText: {
+        fontSize: 14, fontWeight: '600', paddingLeft: 24, paddingTop: 24
+    },
+    visaLogo: { height: 20, width: 60 },
+    visaRow: { flexDirection: 'row', alignItems: 'center', justifyContent: 'flex-end', paddingRight: 24, paddingTop: 10 },
 })
 
 export default styles;
